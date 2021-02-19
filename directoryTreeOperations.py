97d8309d20fdde6375fc065127d2e545de1220fc
TheBeneficent <msn.sdr@gmail.com>
2021-02-19 21:50:36 +0330

    Beta version, No progress bar, no response until finish, filtering and then deleting the filter textbox bug, Move button not working, wont overwrite files if already exist

--- directoryTreeOperations.py
+++ directoryTreeOperations.py
@@ -39,9 +39,34 @@ def fileTree(p, file_types='', dst_drive='',dirtree=True, filetree=True, inc_bas
     str_file_tree = ''
     str_dir_tree=''
     file_with_ext_str = ''
+    dst_file_with_ext_str=''
+    src_size=0
+    src_with_ext_size=0
     if file_types:
-        file_types = tuple(map(str, file_types.replace(' ','').split(',')))
+        file_types = list(map(str, file_types.replace(' ','').split(',')))
     for path, subdirs, files in os.walk(p):
+        if dirtree:
+            for name in subdirs:
+                if path_type == 'windows':
+                    if inc_base:
+                        dir_tree.append(str(os.path.join(base_dir, pathlib.PureWindowsPath(path, name))))
+                        rel_dir_tree.append(str(os.path.join(base_dir, pathlib.PureWindowsPath(dir_tree[j][path_len+1:]))))
+                    else:
+                        dir_tree.append(str( pathlib.PureWindowsPath(path, name)))
+                        rel_dir_tree.append(str(pathlib.PureWindowsPath(dir_tree[j][path_len+1:])))
+                else:
+                    if inc_base:
+                        dir_tree.append(str(os.path.join(base_dir, pathlib.PurePosixPath(path, name))))
+                        rel_dir_tree.append(str(os.path.join(base_dir, pathlib.PurePosixPath(dir_tree[j][path_len+1:]))))
+                    else:
+                        dir_tree.append(str(pathlib.PurePosixPath(path, name)))
+                        rel_dir_tree.append(str(pathlib.PurePosixPath(dir_tree[j][path_len+1:])))
+                
+                if dst_drive:
+                    dst_dir_tree.append( str(os.path.join(dst_drive,rel_dir_tree[j])) )
+
+                str_dir_tree += str(j+1)+') '+str(rel_dir_tree[j])+'\n'
+                j += 1
         if filetree:
             for name in files:
                 if path_type == 'windows':
@@ -68,42 +93,20 @@ def fileTree(p, file_types='', dst_drive='',dirtree=True, filetree=True, inc_bas
                     dst_file_tree.append(str(os.path.join(dst_drive,rel_file_tree[i])) )
                     
                 if file_types:
-                    for ext in file_types:
-                        if not ext.startswith('.'):
-                            ext = '.'+ext
-                        if file_tree[i].lower().endswith(ext):
+                    for extindex in range(0,len(file_types)):
+                        if not file_types[extindex].startswith('.'):
+                            file_types[extindex] = '.'+file_types[extindex]
+                        if file_tree[i].lower().endswith(file_types[extindex]):
                             file_with_ext.append(file_tree[i])
                             file_dir_with_ext.append(file_dir[i])
+                            src_with_ext_size += os.path.getsize(file_tree[i])
                             dst_file_with_ext.append(dst_file_tree[i])
-                            # dst_dir_with_ext.append(dst_dir_tree[i])
+                            dst_dir_with_ext.append(dst_file_tree[i].rpartition(os.sep)[0])
                             
-
+                src_size += os.path.getsize(file_tree[i])
                 str_file_tree += str(i+1)+') '+str(rel_file_tree[i])+'\n'
                 i += 1
-        if dirtree:
-            for name in subdirs:
-                if path_type == 'windows':
-                    if inc_base:
-                        dir_tree.append(str(os.path.join(base_dir, pathlib.PureWindowsPath(path, name))))
-                        rel_dir_tree.append(str(os.path.join(base_dir, pathlib.PureWindowsPath(dir_tree[j][path_len+1:]))))
-                    else:
-                        dir_tree.append(str( pathlib.PureWindowsPath(path, name)))
-                        rel_dir_tree.append(str(pathlib.PureWindowsPath(dir_tree[j][path_len+1:])))
-                else:
-                    if inc_base:
-                        dir_tree.append(str(os.path.join(base_dir, pathlib.PurePosixPath(path, name))))
-                        rel_dir_tree.append(str(os.path.join(base_dir, pathlib.PurePosixPath(dir_tree[j][path_len+1:]))))
-                    else:
-                        dir_tree.append(str(pathlib.PurePosixPath(path, name)))
-                        rel_dir_tree.append(str(pathlib.PurePosixPath(dir_tree[j][path_len+1:])))
-                
-                if dst_drive:
-                    dst_dir_tree.append( str(os.path.join(dst_drive,rel_dir_tree[j])) )
-                if file_types:
-                    dst_dir_with_ext.append(dst_dir_tree[j])            
-
-                str_dir_tree += str(j+1)+') '+str(rel_dir_tree[j])+'\n'
-                j += 1
+        
     l=len(file_with_ext)
     m=0
     while m<l:
@@ -111,10 +114,10 @@ def fileTree(p, file_types='', dst_drive='',dirtree=True, filetree=True, inc_bas
         m+=1
     n=0
     while n<l:
-        file_with_ext_str += str(n+1) + ') '+str(file_with_ext[n])+'\n'
+        dst_file_with_ext_str += str(n+1) + ') '+str(dst_file_with_ext[n])+'\n'
         n+=1
     
-    return {"file_count": i,"dir_count":j, "file_tree": file_tree, "rel_file_tree": rel_file_tree, "rel_file_tree_str": str_file_tree, "dst_file_tree": dst_file_tree, "file_with_ext": file_with_ext, "file_dir_with_ext":file_dir_with_ext, "file_with_ext_str": file_with_ext_str, "dst_file_with_ext":dst_file_with_ext, "dst_dir_with_ext":dst_dir_with_ext, "file_dir":file_dir, "rel_file_dir":rel_file_dir, "dir_tree":dir_tree,"rel_dir_tree":rel_dir_tree,"rel_dir_tree_str":str_dir_tree, "dst_dir_tree":dst_dir_tree}
+    return {"file_count": i,"dir_count":j, "file_with_ext_count":len(file_with_ext), "file_dir_with_ext_count":len(file_dir_with_ext), "file_tree": file_tree, "rel_file_tree": rel_file_tree, "rel_file_tree_str": str_file_tree, "dst_file_tree": dst_file_tree, "file_with_ext": file_with_ext, "file_dir_with_ext":file_dir_with_ext, "file_with_ext_str": file_with_ext_str, "dst_file_with_ext":dst_file_with_ext, "dst_file_with_ext_str":dst_file_with_ext_str, "dst_dir_with_ext":dst_dir_with_ext, "file_dir":file_dir, "rel_file_dir":rel_file_dir, "dir_tree":dir_tree,"rel_dir_tree":rel_dir_tree,"rel_dir_tree_str":str_dir_tree, "dst_dir_tree":dst_dir_tree, "src_size":src_size,"src_with_ext_size":src_with_ext_size}
 
 def deletefile(X):
     for i in X:
@@ -173,11 +176,12 @@ results=''
 dir_check_state=False
 operate_state=''
 win_w = 800
-win_h = 800
+win_h = 650
 y = 20
 h = 25
+t0=15
 t1=25
-t2=30
+t2=25
 t=30
 h2=int(win_h*5/12)
 x1=int(win_w/12)
@@ -204,7 +208,19 @@ src_scroll_view = scrolledtext.ScrolledText(win, wrap=tk.WORD)
 dst_label=tk.Label(text="Destination")
 dst_scroll_view = scrolledtext.ScrolledText(win, wrap=tk.WORD)
 oper_state_scroll_view=scrolledtext.ScrolledText(win,wrap=tk.WORD)
+oper_state_label=tk.Label(text="Operation Status")
 
+def humanReadableFileSize(size_in_bytes,prec=6):
+    if size_in_bytes>=1024**4:
+        return "{:.6f}".format(size_in_bytes/(1024**4))+' TB'
+    elif size_in_bytes>=1024**3:
+        return "{:.6f}".format(size_in_bytes/(1024**3))+' GB'
+    elif size_in_bytes>=1024**2:
+        return "{:.6f}".format(size_in_bytes/(1024**2))+' MB'
+    elif size_in_bytes>=1024:
+        return "{:.6f}".format(size_in_bytes/(1024))+' KB'
+    else:
+        return "{:.6f}".format(size_in_bytes)+' Bytes'
 
 def browsetrig():
     global folder
@@ -242,7 +258,8 @@ def dstbrowsetrig():
         operate_state='Destinatin set!\n'
         results = fileTree(folder,dst_drive=dst_folder)
         tree_scroll_view.delete('1.0', tk.END)
-        tree_scroll_view.insert(tk.INSERT, 'Directories:\n'+results["rel_dir_tree_str"]+'\n\n----------\n\nFiles:\n'+results["rel_file_tree_str"])
+        src_size=humanReadableFileSize(results['src_size'])
+        tree_scroll_view.insert(tk.INSERT, 'Total number of directories: '+str(results['dir_count'])+'\nTotal number of files: '+str(results['file_count'])+'\nTotal size: '+str(src_size)+'\n' + 'Directories:\n'+results["rel_dir_tree_str"]+'\n\n----------\n\nFiles:\n'+results["rel_file_tree_str"])
     oper_state_scroll_view.insert(tk.INSERT, operate_state)
 
 def filtertrig():
@@ -258,10 +275,10 @@ def filtertrig():
     dst_folder=dst_dir_input.get()
     file_extensions=ext_text_input.get()
     results=fileTree(folder,file_types=file_extensions,dst_drive=dst_folder)
+    src_with_ext_size=humanReadableFileSize(results['src_with_ext_size'])
     filtered_scroll_view.delete('1.0', tk.END)
-    filtered_scroll_view.insert(tk.INSERT,results["file_with_ext_str"])
-    oper_state_scroll_view.insert(tk.INSERT, 'Destination files would be like:\n')
-    oper_state_scroll_view.insert(tk.INSERT, results["dst_file_with_ext"])
+    filtered_scroll_view.insert(tk.INSERT,'Total number of directories: '+str(results['file_dir_with_ext_count'])+'\nTotal number of files: '+str(results['file_with_ext_count'])+'\nTotal size: '+src_with_ext_size+'\n' + 'Destination files would be like:\n'+results['dst_file_with_ext_str'])
+    oper_state_scroll_view.insert(tk.INSERT, operate_state)
 
     
 def copytrig():
@@ -383,12 +400,14 @@ filtered_label.place(x=x2,y=y,width=w,height=h)
 y += t1
 tree_scroll_view.place(x=x1, y=y,width=w, height=h2)
 filtered_scroll_view.place(x=x2,y=y,width=w,height=h2)
-y +=t2+h2
+y +=10+h2
 copy_btn.place(x=x1,y=y,width=w_btn,height=h)
 move_btn.place(x=x2-2*x1,y=y,width=w_btn,height=h)
 mkdir_btn.place(x=x2,y=y,width=w_btn*2,height=h)
 filter_btn.place(x=3*x1+x2,y=y,width=w_btn,height=h)
-y += t1
+y += t2
+oper_state_label.place(x=x1,y=y,width=w2,height=h)
+y+=t2
 oper_state_scroll_view.place(x=x1,y=y,width=w2+w_btn,height=3*h)
 
 win.mainloop()
